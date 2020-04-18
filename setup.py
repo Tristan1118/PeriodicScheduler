9039dc570862e574bc6bcd617c21c5cb59e434c0
tristan.ebert <tristan.ebert@googlemail.com>
2020-04-18 17:21:20 +0200

    Added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pyservice',
+    version='1.0',
+    description='Toolbox for running services with python.',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License'
+)
